13fff77440015e4a6f8c6f2130c9f0fee96547e6
anass <anass.belcaid@gmail.com>
2018-08-11 16:55:56 +0100

    configuration for the ipython startup commands

---
+++ ipython/00_imports.py
@@ -0,0 +1,11 @@
+#default imports
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+#matplotlib options
+plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
